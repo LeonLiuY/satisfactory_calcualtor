c8b3c2a21e052c8958afd42afc66f689e9936a07
Leon Liu <leon.liuyang.d@gmail.com>
2025-06-29 13:56:11 +0000

    update

--- service-worker.ts
+++ service-worker.ts
@@ -1,26 +1,34 @@
 
 const CACHE_NAME = "v1";
-const ALWAYS_FETCH_PATHS: string[] = ["/", "/index.html"];
+const ALWAYS_FETCH_PATHS: string[] = ["/", "/index.html", "/service-worker.js"];
+const ALWAYS_CACHE_EXTS: string[] = [".js", ".css", ".wasm"];
 
 self.addEventListener("fetch", (event: FetchEvent) => {
   const url = new URL(event.request.url);
   if (ALWAYS_FETCH_PATHS.includes(url.pathname)) {
+    console.log(`fetch ${url.pathname}`)
     event.respondWith(
       fetch(event.request)
         .then(response => {
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, response.clone()));
-          return response;
+          console.log("")
+          caches.open(CACHE_NAME).then(cache => {
+            console.log(`cache ${url.pathname}`);
+            cache.put(event.request, response)
+          });
+          return response.clone();
         })
         .catch(() => caches.match(event.request))
     );
-  } else {
+  } else if (ALWAYS_CACHE_EXTS.some(ext => url.pathname.endsWith(ext))) {
+    console.log(`match ${url.pathname}`)
     event.respondWith(
-      caches.match(event.request).then(response =>
-        response || fetch(event.request).then(fetchRes => {
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, fetchRes.clone()));
-          return fetchRes;
+      caches.match(event.request).then(response => {
+        console.log(`match ${response}`);
+        return response || fetch(event.request).then(fetchRes => {
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, fetchRes));
+          return fetchRes.clone();
         })
-      )
+      })
     );
   }
 });
