58883d1cad5661d5403548c31b021edf97799963
Leon Liu <leon.liuyang.d@gmail.com>
2025-06-29 13:56:35 +0000

    update

--- service-worker.ts
+++ service-worker.ts
@@ -6,13 +6,10 @@ const ALWAYS_CACHE_EXTS: string[] = [".js", ".css", ".wasm"];
 self.addEventListener("fetch", (event: FetchEvent) => {
   const url = new URL(event.request.url);
   if (ALWAYS_FETCH_PATHS.includes(url.pathname)) {
-    console.log(`fetch ${url.pathname}`)
     event.respondWith(
       fetch(event.request)
         .then(response => {
-          console.log("")
           caches.open(CACHE_NAME).then(cache => {
-            console.log(`cache ${url.pathname}`);
             cache.put(event.request, response)
           });
           return response.clone();
@@ -20,10 +17,8 @@ self.addEventListener("fetch", (event: FetchEvent) => {
         .catch(() => caches.match(event.request))
     );
   } else if (ALWAYS_CACHE_EXTS.some(ext => url.pathname.endsWith(ext))) {
-    console.log(`match ${url.pathname}`)
     event.respondWith(
       caches.match(event.request).then(response => {
-        console.log(`match ${response}`);
         return response || fetch(event.request).then(fetchRes => {
           caches.open(CACHE_NAME).then(cache => cache.put(event.request, fetchRes));
           return fetchRes.clone();
